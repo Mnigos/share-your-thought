4f7b0d0aefb12fdde4ba8e7e8cdcb56235a8f744
Mnigos <makowskiigor@gmail.com>
2024-09-26 20:46:20 +0200

    feat(apps/web/thoughts/layout): add dropdown menu with logout button

--- apps/web/app/thoughts/layout.tsx
+++ apps/web/app/thoughts/layout.tsx
@@ -1,9 +1,18 @@
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { User } from 'lucide-react'
+import { User, LogOut } from 'lucide-react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@repo/ui/components/dropdown-menu'
+import { Button } from '@repo/ui/components/button'
+import { unstable_cache } from 'next/cache'
 
 import { trpc } from '../trpc'
 import type { LayoutProps } from '../types/props'
+import { logout } from '../actions/auth'
 
 export const runtime = 'edge'
 
@@ -12,15 +21,35 @@ export default async function ThoughtsLayout({ children }: LayoutProps) {
 
   if (!username) redirect('/thoughts')
 
-  const user = await trpc.user.byName.query({ name: username })
+  const getUserByName = unstable_cache(
+    (name: string) => trpc.user.byName.query({ name }),
+    [username]
+  )
+
+  const user = await getUserByName(username)
 
   return (
     <div>
       <nav className="flex w-full justify-end bg-black px-4 py-2">
-        <div className="flex items-center gap-2">
-          <p className="text-lg">{user.name}</p>
-          <User />
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex items-center gap-2" asChild>
+            <Button variant="outline">
+              <p className="text-lg">{user.name}</p>
+              <User />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <form action={logout}>
+              <DropdownMenuItem className="flex items-center gap-2" asChild>
+                <Button variant="destructive" className="w-full" type="submit">
+                  <LogOut />
+                  Logout
+                </Button>
+              </DropdownMenuItem>
+            </form>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </nav>
 
       <main>{children}</main>
