4596c5fe122ee7b744d2374e651793a68f423eab
Mnigos <makowskiigor@gmail.com>
2024-09-26 18:43:37 +0200

    chore(apps/api/prisma): move database connection to `schema.prisma`

--- apps/api/src/config/env/env.schema.ts
+++ apps/api/src/config/env/env.schema.ts
@@ -2,11 +2,12 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(4000),
-  DATABASE_HOST: z.coerce.string(),
+  DATABASE_HOST: z.string(),
   DATABASE_PORT: z.coerce.number().optional().default(5432),
-  DATABASE_USERNAME: z.coerce.string(),
-  DATABASE_PASSWORD: z.coerce.string(),
-  DATABASE_NAME: z.coerce.string(),
+  DATABASE_USERNAME: z.string(),
+  DATABASE_PASSWORD: z.string(),
+  DATABASE_NAME: z.string(),
+  DATABASE_URL: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
