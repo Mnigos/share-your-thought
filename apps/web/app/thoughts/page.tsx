19c510d8479dd5204ae206749458d73c50c73cbc
Mnigos <makowskiigor@gmail.com>
2024-09-27 15:55:56 +0200

    feat(apps/web/thoughts): implement getting and creating thoughts

--- apps/web/app/thoughts/page.tsx
+++ apps/web/app/thoughts/page.tsx
@@ -1,7 +1,28 @@
-export default function ThoughtsPage() {
+import { getThoughts } from '../actions/thoughts'
+import { CreateThoughtForm, ThoughtCard } from '../components/thoughts'
+
+export default async function ThoughtsPage() {
+  const thoughts = await getThoughts()
+
   return (
-    <div>
+    <div className="p-4">
       <h1>Thoughts</h1>
+
+      <div className="mx-auto max-w-[1100px]">
+        <CreateThoughtForm />
+
+        <section>
+          <header>
+            <h2>Recent thoughts</h2>
+          </header>
+
+          <div className="flex flex-col gap-2">
+            {thoughts.reverse().map(({ id, content, author }) => (
+              <ThoughtCard key={id} content={content} author={author} />
+            ))}
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
