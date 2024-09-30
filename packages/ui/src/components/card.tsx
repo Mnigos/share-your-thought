1ce0fbf01caa00531921f89a58095d1408a05d67
Mnigos <makowskiigor@gmail.com>
2024-09-30 20:31:35 +0200

    refactor(packages/ui/components): change styling

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'bg-card text-card-foreground rounded-lg border p-2 shadow-sm',
+      'bg-card text-card-foreground rounded-lg p-2 shadow-sm',
       className
     )}
     {...props}
