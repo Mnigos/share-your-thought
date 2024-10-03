03452fc09474b586bf21faf320ac072ee1877c29
Mnigos <makowskiigor@gmail.com>
2024-10-03 12:21:28 +0200

    feat(apps/api/main): enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from '~/app'
 import { EnvService } from '~/config/env'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  })
 
   const envService = app.get(EnvService)
 
