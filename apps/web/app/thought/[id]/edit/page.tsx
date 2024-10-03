0f99425d29fc7297955c62ac37d06e15ba2d7ce8
Mnigos <makowskiigor@gmail.com>
2024-10-03 17:50:18 +0200

    feat(apps/web/app/thought/edit/page): show edit thought modal

--- apps/web/app/thought/[id]/edit/page.tsx
+++ apps/web/app/thought/[id]/edit/page.tsx
@@ -1,3 +1,30 @@
-export default function EditThoughtPage() {
-  return null
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+} from '@repo/ui/components/card'
+
+import type { EditThoughtPageProps } from '~/app/types/props'
+import { ThoughtForm } from '~/components/thoughts'
+import { getThoughtById } from '~/server/actions/thoughts'
+
+export default async function EditThoughtPage({
+  params,
+}: EditThoughtPageProps) {
+  const thought = await getThoughtById(params.id)
+
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <Card className="flex w-full flex-col gap-8 p-4 md:w-[512px]">
+        <CardHeader>
+          <CardTitle className="text-center">Edit thought</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <ThoughtForm thought={thought} />
+        </CardContent>
+      </Card>
+    </main>
+  )
 }
