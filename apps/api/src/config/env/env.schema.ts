cad7e576565f947aa02d054c8ebd99201df08b90
Mnigos <makowskiigor@gmail.com>
2024-10-03 10:56:34 +0200

    refactor(apps/api/config/env/schema): make db variables optional

--- apps/api/src/config/env/env.schema.ts
+++ apps/api/src/config/env/env.schema.ts
@@ -4,9 +4,9 @@ export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(4000),
   DATABASE_HOST: z.string(),
   DATABASE_PORT: z.coerce.number().optional().default(5432),
-  DATABASE_USERNAME: z.string(),
-  DATABASE_PASSWORD: z.string(),
-  DATABASE_NAME: z.string(),
+  DATABASE_USERNAME: z.string().optional(),
+  DATABASE_PASSWORD: z.string().optional(),
+  DATABASE_NAME: z.string().optional(),
   DATABASE_URL: z.string(),
 })
 
